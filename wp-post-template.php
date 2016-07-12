a34c492d85a688798bb627031f8afa9f5107f1bb
mithublue <cemithu06@gmail.com>
2016-07-12 12:32:06 +0600

    Plugin final

--- wp-post-template.php
+++ wp-post-template.php
@@ -14,10 +14,13 @@ class WPPT_Init {
     public function __construct() {
         add_action('add_meta_boxes', array( $this, 'wp_add_post_custom_template' ) );
         add_action('save_post', array( $this, 'wp_save_custom_post_template' ),10,2);
+        add_filter('single_template', array( $this, 'wp_load_custom_post_template' ));
     }
 
 
     function wp_add_post_custom_template( $post_type ) {
+
+        if( $post_type == 'page') return;
 
         add_meta_box(
             'postparentdiv',
@@ -32,6 +35,7 @@ class WPPT_Init {
     function wp_custom_post_template_meta_box( $post ) {
 
         $templates = get_page_templates();
+        $sel_template = get_post_meta( $post->ID, '_wp_page_template', true );
         ?>
         <p><strong>Template</strong></p>
         <label class="screen-reader-text" for="page_template">Page Template</label>
@@ -39,16 +43,33 @@ class WPPT_Init {
         <select name="page_template" id="page_template">
             <option value="default">Default Template</option>
             <?php foreach( $templates as $name => $path ):?>
-                <option value="<?php echo $path; ?>"><?php echo $name; ?></option>
+                <option value="<?php echo $path; ?>" <?php echo $sel_template == $path ? 'selected' : ''; ?>><?php echo $name; ?></option>
             <?php endforeach;?>
         </select>
         <?php endif; ?>
     <?php
     }
 
-    function wp_save_custom_post_template( $post_id, $post) {
-        if ($post->post_type !='page' && !empty($_POST['page_template'])) {
+    function wp_load_custom_post_template( $single_template ) {
+        global $post;
+        $template_path = get_post_meta( $post->ID , '_wp_page_template', true );
 
+        if( $template_path && $template_path != 'default' ) {
+            if( $custom_template = locate_template($template_path) )
+                return $custom_template;
+        }
+
+        return $single_template;
+    }
+
+    function wp_save_custom_post_template( $post_id, $post ) {
+
+        if ( get_post_type( $post_id ) == 'page' ) return;
+
+        if( !current_user_can( 'edit_post' ) ) return;
+
+        if ($post->post_type !='page' && !empty($_POST['page_template'])) {
+            update_post_meta( $post_id, '_wp_page_template', $_POST['page_template'] );
         }
     }
     public static function init() {
